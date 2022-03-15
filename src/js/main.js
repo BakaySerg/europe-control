85df518471177019380668aa0786f0bcce0a0024
Serg Bakay <bakay.sergey28@gmail.com>
2022-03-15 21:45:44 +0200

    footer - 70%

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,12 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 
+
+	// document.querySelectorAll('.slider__item').forEach(item => {
+	// 	item.addEventListener('click', event => {
+	// 		item.classList.toggle('opened')
+	// 	})
+	// })
+
+
 });
