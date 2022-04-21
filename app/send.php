477c2dc38641bf89f7864b3087e87a1ea0c4270a
Serg Bakay <bakay.sergey28@gmail.com>
2022-04-21 13:15:57 +0300

    order-service 90% (begin send form)

---
+++ app/send.php
@@ -0,0 +1,26 @@
+<?php
+
+function mail_utf8($to, $from, $subject='(No subject)', $message='', $add_header='') {
+    $headers = "MIME-Version: 1.0\r\n" . "Content-type: text/plain; charset=UTF-8\r\n" . "From: $from\r\n" . $add_header;
+    mail($to, '=?UTF-8?B?'.base64_encode($subject).'?=', $message, $headers);
+}
+
+$recepient = 'bakay.sergey28@gmail.com';  //*self Email*/
+// $recepient = 'shelltown@gmail.com';  //*self Email*/
+$sitename = "europe-control";
+
+$name = trim($_POST["name"]);
+$tel = trim($_POST["phone"]);
+$from = trim($_POST["email"]);
+$company = trim($_POST["company"]);
+$message = trim($_POST["msg"]);
+$subject = "New contact";
+$ip = $_SERVER["REMOTE_ADDR"];
+
+$message = "I wish to contact you, $subject\nName: $name\nTel: $tel\nE-mail: $from \nCompany: $company\nMessage: $message\n(User IP:  $ip)";
+
+$pagetitle = 'have a new contact with the site "'.$sitename.'"';
+
+mail_utf8($recepient, $from, $pagetitle, $message, "Reply-To: $from\r\n");
+
+?>
